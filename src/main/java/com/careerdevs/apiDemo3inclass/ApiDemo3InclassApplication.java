631df9b4d701de31b928c622f301dde0ae4ec8a0
GabrielQZ <gabrielquinn.photography@gmail.com>
2022-02-28 21:03:31 -0500

    Initial commit

---
+++ src/main/java/com/careerdevs/apiDemo3inclass/ApiDemo3InclassApplication.java
@@ -0,0 +1,13 @@
+package com.careerdevs.apiDemo3inclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDemo3InclassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDemo3InclassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/careerdevs/apiDemo3inclass/ApiDemo3InclassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerdevs.apiDemo3inclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDemo3InclassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
